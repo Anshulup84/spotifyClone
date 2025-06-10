ad6f9b36f30aebd3d6adc25f544283e061c897fa
Anshul <avenger720720@gmail.com>
2025-06-10 18:04:20 +0530

    working on  song functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+async function main (){
+    let a = await fetch("http://127.0.0.1:3000/spotifyClone/songs/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let tds = div.getElementsByTagName("td")
+    console.log(tds);
+    
+}
+
+main()
