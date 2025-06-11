ae6dcd617b3d7343397bd159039c8937b060d1f4
Anshul <avenger720720@gmail.com>
2025-06-11 20:06:00 +0530

    getsong functionality

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-async function main (){
+async function getSongs (){
+    
     let a = await fetch("http://127.0.0.1:3000/spotifyClone/songs/")
     let response = await a.text();
     console.log(response);
     let div = document.createElement("div")
     div.innerHTML = response
-    let tds = div.getElementsByTagName("td")
-    console.log(tds);
+    let as = div.getElementsByTagName("a")
+    
+    let songs = []
+    for(let index = 0; index < as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
     
 }
+
+async function main(){
+let songs = await getSongs()
+console.log(songs);
+
+}
+
 
 main()
