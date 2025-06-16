b0ca34274102f4865e90b80e6b5a6dcdce4dae47
Anshul <avenger720720@gmail.com>
2025-06-16 17:02:34 +0530

    song list layout completed

--- script.js
+++ script.js
@@ -25,7 +25,20 @@ console.log(songs);
 
    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
    for(const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+    songUL.innerHTML = songUL.innerHTML + `
+    <li>
+              <img class="invert" src="music.svg" alt="">
+              <div class="info">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>Anshu</div>
+              </div>
+              <div class="playnow">
+                <span>Play Now</span>
+                <img class="invert" src="play.svg" alt="">
+              </div>
+
+           
+    </li>`;
    }
 
     //Play the first song
