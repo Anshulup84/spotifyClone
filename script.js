60636e38cc254d109e78695a2d8416931ce14566
Anshul <avenger720720@gmail.com>
2025-06-17 17:17:17 +0530

    making seekbar completed

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-async function getSongs (){
-    
-    let a = await fetch("http://127.0.0.1:3000/spotifyClone/songs/")
-    let response = await a.text();
-    console.log(response);
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    
-    let songs = []
-    for(let index = 0; index < as.length; index++){
-        const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("/songs/")[1])
-        }
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:3000/spotifyClone/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
     }
-    return songs
-    
+  }
+  return songs;
 }
 
-async function main(){
-    // Get the list of all ssongs
-let songs = await getSongs()
-console.log(songs);
+async function main() {
+  // Get the list of all ssongs
+  let songs = await getSongs();
 
-   let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-   for(const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `
+  // show all the songs in the playlist
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `
     <li>
               <img class="invert" src="music.svg" alt="">
               <div class="info">
@@ -35,23 +37,9 @@ console.log(songs);
               <div class="playnow">
                 <span>Play Now</span>
                 <img class="invert" src="play.svg" alt="">
-              </div>
-
-           
+              </div>        
     </li>`;
-   }
-
-    //Play the first song
-    var audio = new Audio(songs[0])
-    // audio.play();
-
-    audio.addEventListener("loadeddata", () => {
-        
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
-        
-        
-    })
+  }
 }
 
-
-main()
+main();
