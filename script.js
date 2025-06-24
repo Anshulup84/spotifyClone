d7fe16c2820c8eaf263a613ef7f7f7ca6026f02a
Anshul <avenger720720@gmail.com>
2025-06-24 17:44:50 +0530

    hamburger functionality completed

--- script.js
+++ script.js
@@ -105,6 +105,14 @@ async function main() {
   })
 
   //Add an event listener for hamburger
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "0"
+  })
+
+  // Add an event listener for close
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-120%"
+  })
 
 }
 
